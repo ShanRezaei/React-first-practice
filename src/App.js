fed6f12ebe543b389e1b946fbfc5c71add01c24d
shan rezaei <dew.rezaei@gmail.com>
2023-11-16 12:38:26 -0500

    first app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  // example of how to use state
+  
+
+  return (
+    // we will have 2 parts, first part will contains input and the button 
+    //second part contains the list
+  <div className="App">
+      <div className='AddTask'>
+        <input />
+        <button>Add Task</button>
+      </div>
+      <div className='list'>
+        
+      </div>
+  </div>
+  );}
+
+  export default App;
+
+
+  
